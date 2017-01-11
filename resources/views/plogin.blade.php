c4ad832d8ae7bb3f1a2cadc2e3dadbfe81be2797
saurabh-neo <saurabhmatrixneo@gmail.com>
2017-01-11 05:47:39 +0530

    plogin.blade.php changed for UI logo bug

---
+++ resources/views/plogin.blade.php
@@ -0,0 +1,405 @@
+
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Healthcare</title>
+		<meta name="description" content="Free Bootstrap Theme by BootstrapMade.com">
+		<meta name="keywords" content="free website templates, free bootstrap themes, free template, free bootstrap, free website template">
+		
+		<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Open+Sans|Raleway|Candal">
+		<link rel="stylesheet" type="text/css" href="{{asset('dist/css/font-awesome.min.css')}}">
+		<link rel="stylesheet" type="text/css" href="{{asset('dist/css/bootstrap.min.css')}}">
+		<link rel="stylesheet" type="text/css" href="{{asset('dist/css/style.css')}}">
+
+		</head>
+	<body id="myPage" data-spy="scroll" data-target=".navbar" data-offset="60">
+		<!--banner-->
+		<section id="banner" class="banner">
+			<div class="bg-color">
+				<nav class="navbar navbar-default navbar-fixed-top">
+					<div class="container">
+						<div class="col-md-12">
+							<div class="navbar-header">
+								<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+									<span class="icon-bar"></span>
+									<span class="icon-bar"></span>
+									<span class="icon-bar"></span>
+								</button>
+								<a class="navbar-brand" href="#"><img src="{{asset('dist/img/logo.png')}}" class="img-responsive" style="width: 140px; margin-top: -16px;"></a>
+							</div>
+							<div class="collapse navbar-collapse navbar-right" id="myNavbar">
+								<ul class="nav navbar-nav">
+									<li class="active"><a href="#banner">Home</a></li>
+									<li class=""><a href="#service">Services</a></li>
+									<li class=""><a href="#doctor-team">Doctors-Team</a></li>
+									<li class=""><a href="#testimonial">Testimonial</a></li>
+									<li class=""><a href="#footer">Contact</a></li>
+								</ul>
+							</div>
+						</div>
+					</div>
+				</nav>
+				<div class="container">
+					<div class="row">
+						<div class="banner-info">
+							<div class="banner-logo text-center">
+								<img src="{{asset('dist/img/logo.png')}}" style="margin-top:-140px;" class="img-responsive">
+							</div>
+							<div class=" text-center" style="margin-top:-50px;">
+								<h1 class="white"><strong>Healthcare at your desk!! We Care For You <i class="fa fa-heartbeat" style="color:deeppink" aria-hidden="true"></i></strong></h1><br>
+							</div>
+															
+									<div class="container" style="margin-left:300px;">
+										<form class="form-signin col-md-4">
+											<h2 class="banner-text text-center form-signin-heading">Patient Login</h2><br>
+											<label for="inputEmail" class="sr-only">Patient_ID</label>
+											<input type="text" style="background:transparent" id="inputEmail" class="form-control" placeholder="Patient_ID" required="" autofocus=""><br>
+											<label for="inputPassword" class="sr-only">Password</label>
+											<input type="password" style="background:transparent" id="inputPassword" class="form-control" placeholder="Password" required="">
+											<br><button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+										</form>
+									</div>
+								
+							<div class="overlay-detail text-center">
+								<a href="#service"><i class="fa fa-angle-down"></i></a>
+							</div>		
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+		<!--/ banner-->
+		<!--service-->
+		<section id="service" class="section-padding">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-4 col-sm-4">
+						<h2 class="ser-title">Our Service</h2>
+						<hr class="botm-line">
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris cillum.</p>
+					</div>
+					<div class="col-md-4 col-sm-4">
+						<div class="service-info">
+							<div class="icon">
+								<i class="fa fa-stethoscope"></i>
+							</div>
+							<div class="icon-info">
+								<h4>24 Hour Support</h4>
+								<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+							</div>
+						</div>
+						<div class="service-info">
+							<div class="icon">
+								<i class="fa fa-ambulance"></i>
+							</div>
+							<div class="icon-info">
+								<h4>Emergency Services</h4>
+								<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-4 col-sm-4">
+						<div class="service-info">
+							<div class="icon">
+								<i class="fa fa-user-md"></i>
+							</div>
+							<div class="icon-info">
+								<h4>Medical Counseling</h4>
+								<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+							</div>
+						</div>
+						<div class="service-info">
+							<div class="icon">
+								<i class="fa fa-medkit"></i>
+							</div>
+							<div class="icon-info">
+								<h4>Premium Healthcare</h4>
+								<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+		<!--/ service-->
+		<!--cta-->
+		<section id="cta-1" class="section-padding">
+			<div class="container">
+				<div class="row">
+					<div class="schedule-tab">
+						<div class="col-md-4 col-sm-4 bor-left">
+							<div class="mt-boxy-color"></div>
+							<div class="medi-info">
+								<h3>Emergency Case</h3>
+								<p>I am text block. Edit this text from Appearance / Customize / Homepage header columns. Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
+								<a href="#" class="medi-info-btn">READ MORE</a>		    	
+							</div>
+						</div>
+						<div class="col-md-4 col-sm-4">
+							<div class="medi-info">
+								<h3>Emergency Case</h3>
+								<p>I am text block. Edit this text from Appearance / Customize / Homepage header columns. Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
+								<a href="#" class="medi-info-btn">READ MORE</a>	
+							</div>
+						</div>
+						<div class="col-md-4 col-sm-4 mt-boxy-3">
+							<div class="mt-boxy-color"></div>
+							<div class="time-info">
+								<h3>Opening Hours</h3>
+								<table style="margin: 8px 0px 0px;" border="1">
+									<tbody>
+										<tr>
+											<td>Monday - Friday</td>
+											<td>8.00 - 17.00</td>
+										</tr>
+										<tr>
+											<td>Saturday</td>
+											<td>9.30 - 17.30</td>
+										</tr>
+										<tr>
+											<td>Sunday</td>
+											<td>9.30 - 15.00</td>
+										</tr>
+									</tbody>
+								</table>				    		
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+		<!--cta-->
+		
+		
+		<!--about-->
+		<section id="about" class="section-padding">
+			<div class="container">
+				<div class="row">
+				</div>
+			</div>
+		</section>
+		<!--/ about-->
+		
+		
+		
+		<!--doctor team-->
+		<section id="doctor-team" class="section-padding">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12">
+						<h2 class="ser-title">Meet Our Doctors!</h2>
+						<hr class="botm-line">
+					</div>
+					<div class="col-md-3 col-sm-3 col-xs-6">
+						<div class="thumbnail"> 
+							<img src="img/doctor1.jpg" alt="..." class="team-img">
+							<div class="caption">
+								<h3>Jessica Wally</h3>
+								<p>Doctor</p>
+								<ul class="list-inline">
+									<li><a href="#"><i class="fa fa-facebook"></i></a></li>
+									<li><a href="#"><i class="fa fa-twitter"></i></a></li>
+									<li><a href="#"><i class="fa fa-google-plus"></i></a></li>
+								</ul>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-3 col-sm-3 col-xs-6">
+						<div class="thumbnail"> 
+							<img src="img/doctor2.jpg" alt="..." class="team-img">
+							<div class="caption">
+								<h3>Iai Donas</h3>
+								<p>Doctor</p>
+								<ul class="list-inline">
+									<li><a href="#"><i class="fa fa-facebook"></i></a></li>
+									<li><a href="#"><i class="fa fa-twitter"></i></a></li>
+									<li><a href="#"><i class="fa fa-google-plus"></i></a></li>
+								</ul>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-3 col-sm-3 col-xs-6">
+						<div class="thumbnail"> 
+							<img src="img/doctor3.jpg" alt="..." class="team-img">
+							<div class="caption">
+								<h3>Amanda Denyl</h3>
+								<p>Doctor</p>
+								<ul class="list-inline">
+									<li><a href="#"><i class="fa fa-facebook"></i></a></li>
+									<li><a href="#"><i class="fa fa-twitter"></i></a></li>
+									<li><a href="#"><i class="fa fa-google-plus"></i></a></li>
+								</ul>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-3 col-sm-3 col-xs-6">
+						<div class="thumbnail"> 
+							<img src="img/doctor4.jpg" alt="..." class="team-img">
+							<div class="caption">
+								<h3>Jason Davis</h3>
+								<p>Doctor</p>
+								<ul class="list-inline">
+									<li><a href="#"><i class="fa fa-facebook"></i></a></li>
+									<li><a href="#"><i class="fa fa-twitter"></i></a></li>
+									<li><a href="#"><i class="fa fa-google-plus"></i></a></li>
+								</ul>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+		<!--/ doctor team-->
+		<!--testimonial-->
+		<section id="testimonial" class="section-padding">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12">
+						<h2 class="ser-title">see what patients are saying?</h2>
+						<hr class="botm-line">
+					</div>
+					<div class="col-md-4 col-sm-4">
+						<div class="testi-details">
+							<!-- Paragraph -->
+							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+						</div>
+						<div class="testi-info">
+							<!-- User Image -->
+							<a href="#"><img src="img/thumb.png" alt="" class="img-responsive"></a>
+							<!-- User Name -->
+							<h3>Alex<span>Texas</span></h3>
+						</div>
+					</div>
+					<div class="col-md-4 col-sm-4">
+						<div class="testi-details">
+							<!-- Paragraph -->
+							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+						</div>
+						<div class="testi-info">
+							<!-- User Image -->
+							<a href="#"><img src="img/thumb.png" alt="" class="img-responsive"></a>
+							<!-- User Name -->
+							<h3>Alex<span>Texas</span></h3>
+						</div>
+					</div>
+					<div class="col-md-4 col-sm-4">
+						<div class="testi-details">
+							<!-- Paragraph -->
+							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+						</div>
+						<div class="testi-info">
+							<!-- User Image -->
+							<a href="#"><img src="img/thumb.png" alt="" class="img-responsive"></a>
+							<!-- User Name -->
+							<h3>Alex<span>Texas</span></h3>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+		<!--/ testimonial-->
+		<!--cta 2-->
+		<section id="cta-2" class="section-padding">
+			<div class="container">
+				<div class=" row">
+					<div class="col-md-2"></div>
+					<div class="text-right-md col-md-4 col-sm-4">
+						<h2 class="section-title white lg-line">« A few words<br> about us »</h2>
+					</div>
+					<div class="col-md-4 col-sm-5">
+						We Care for you 24x7 <i class="fa fa-heartbeat" style="color:red" aria-hidden="true"></i>
+						<p class="text-right text-primary"><i>— Healthcare</i></p>
+					</div>
+					<div class="col-md-2"></div>
+				</div>
+			</div>
+		</section>
+		<!--cta-->
+		<!--contact-->
+		<section id="contact" class="section-padding">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12">
+						<h2 class="ser-title">Contact us</h2>
+						<hr class="botm-line">
+					</div>
+					<div class="col-md-4 col-sm-4">
+						<h3>Contact Info</h3>
+						<div class="space"></div>
+						<p><i class="fa fa-map-marker fa-fw pull-left fa-2x"></i>sunderbaug colony<br>
+						dapodi, pune</p>
+						<div class="space"></div>
+						<p><i class="fa fa-envelope-o fa-fw pull-left fa-2x"></i>saurabh24p@gmail.com</p>
+						<div class="space"></div>
+						<p><i class="fa fa-phone fa-fw pull-left fa-2x"></i>+91 8793829287</p>
+					</div>
+					<div class="col-md-8 col-sm-8 marb20">
+						<div class="contact-info">
+							<h3 class="cnt-ttl">Having Any Query! Or Book an appointment</h3>
+							<div class="space"></div>
+                            <div id="sendmessage">Your message has been sent. Thank you!</div>
+                            <div id="errormessage"></div>
+							<form action="" method="post" role="form" class="contactForm">
+							    <div class="form-group">
+                                    <input type="text" name="name" class="form-control br-radius-zero" id="name" placeholder="Your Name" data-rule="minlen:4" data-msg="Please enter at least 4 chars" />
+                                    <div class="validation"></div>
+								</div>
+                                <div class="form-group">
+                                    <input type="email" class="form-control br-radius-zero" name="email" id="email" placeholder="Your Email" data-rule="email" data-msg="Please enter a valid email" />
+                                    <div class="validation"></div>
+								</div>
+                                <div class="form-group">
+                                    <input type="text" class="form-control br-radius-zero" name="subject" id="subject" placeholder="Subject" data-rule="minlen:4" data-msg="Please enter at least 8 chars of subject" />
+                                    <div class="validation"></div>
+								</div>
+                                <div class="form-group">
+                                    <textarea class="form-control br-radius-zero" name="message" rows="5" data-rule="required" data-msg="Please write something for us" placeholder="Message"></textarea>
+                                    <div class="validation"></div>
+								</div>
+                                
+								<div class="form-action">
+									<button type="submit" class="btn btn-form">Send Message</button>
+								</div>
+							</form>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+		<!--/ contact-->
+		
+		
+		<!--footer-->
+		<footer id="footer">
+			<div class="top-footer">
+				<div class="container">
+					<div class="row">
+						
+					</div>
+				</div>
+			</div>
+			<div class="footer-line">
+				<div class="container">
+					<div class="row">
+						<div class="col-md-12 text-center">
+							
+							<div class="credits" style="margin-bottom:10px">
+								Developed from <i class="fa fa-heartbeat" style="color:hotpink" aria-hidden="true"></i> by <a href="https://saurabh-neo.github.io/">©Saurabh Harwadikar</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</footer>
+		<!--/ footer-->
+		
+		
+		<script src="dist/js/jquery.min.js"></script>
+		<script src="dist/js/jquery.easing.min.js"></script>
+		<script src="dist/js/bootstrap.min.js"></script>
+		<script src="dist/js/custom.js"></script>
+		
+	</body>
+</html>
